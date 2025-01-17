b76518213f78b76c18f9f6089d694cde95c82fe3
Bipin-Naik <bipinnaik32@gmail.com>
2025-01-17 14:56:57 +0530

    Updated urls.py file

--- roll/urls.py
+++ roll/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add-record/', views.add_record, name='add_record'),
     path('search-records/', views.search_records, name='search_records'),
-   # path('send-sms/', views.send_sms, name='send_sms'),
+    path('send-sms/', views.send_sms, name='send_sms'),
     path('return-to-dashboard/', views.return_to_dashboard, name='return_to_dashboard'),
     path('suggest-voter-id/', views.suggest_voter_id, name='suggest_voter_id'),
 ]
